8db0432c56b3f8df90bc49e7d366fc9af396c247
Ravi Kalla <ravi2523096@gmail.com>
2015-12-05 20:11:34 -0500

    Detailed problem statement

--- StudentsTest/src/CompareFiles.java
+++ StudentsTest/src/CompareFiles.java
@@ -2,7 +2,6 @@
 /**
  * Write logic in "findDifferences(...)" function to compare files
  * 
- * @author Ravi_Kalla
  *
  */
 public class CompareFiles {
@@ -13,7 +12,7 @@ public class CompareFiles {
 		System.out.println("Comparison results : \n " + strComparisonResultFileContent);
 	}
 
-	private static String findDifferences(String strFileName1,
+	public static String findDifferences(String strFileName1,
 			String strFileName2) {
 		// TODO : Write logic to compare two files and return the comparison results
 		return null;
@@ -22,44 +21,48 @@ public class CompareFiles {
 
 /*
 Eg1:
-	File1 Content :
-		Employee1,111,Manager
-		Employee3,333,Developer
-		Employee4,444,Tester
-	File2 Content :
-		Employee1,111,Manager
-		Employee2,222,ProjectLead
-		Employee4,444,Tester
+	I/P:
+		File1 Content :
+			Employee1,111,Manager
+			Employee3,333,Developer
+			Employee4,444,Tester
+		File2 Content :
+			Employee1,111,Manager
+			Employee2,222,ProjectLead
+			Employee4,444,Tester
 
-	ComparisonResultFile Content:
-		Employee1,111,Manager
-		// Start : Difference exist between File1 and File2
-		// First file content
-		Employee3,333,Developer
-		// Second file content
-		Employee2,222,ProjectLead
-		// End : Difference exist between File1 and File2
-		Employee4,444,Tester
+	O/P:
+		Comparison results : 
+			Employee1,111,Manager
+			// Start : Difference exist between File1 and File2
+			// First file content
+			Employee3,333,Developer
+			// Second file content
+			Employee2,222,ProjectLead
+			// End : Difference exist between File1 and File2
+			Employee4,444,Tester
 
 Eg2:
-	File1 Content :
-		Employee1,111,Manager
-		Employee2,222,ProjectLead
-		Employee3,333,Developer
-		Employee4,444,Tester
-	File2 Content :
-		Employee1,111,Manager
-		Employee2,222,ProjectLead
-		Employee4,444,Tester
+	I/P:
+		File1 Content :
+			Employee1,111,Manager
+			Employee2,222,ProjectLead
+			Employee3,333,Developer
+			Employee4,444,Tester
+		File2 Content :
+			Employee1,111,Manager
+			Employee2,222,ProjectLead
+			Employee4,444,Tester
 
-	ComparisonResultFile Content:
-		Employee1,111,Manager
-		Employee2,222,ProjectLead
-		// Start : Difference exist between File1 and File2
-		// First file content
-		Employee3,333,Developer
-		// Second file content
-		Employee3,333,Developer
-		// End : Difference exist between File1 and File2
-		Employee4,444,Tester
+	O/P:
+		Comparison results :
+			Employee1,111,Manager
+			Employee2,222,ProjectLead
+			// Start : Difference exist between File1 and File2
+			// First file content
+			Employee3,333,Developer
+			// Second file content
+			-----------------------
+			// End : Difference exist between File1 and File2
+			Employee4,444,Tester
 */
--- StudentsTest/src/ExpressionEvaluation.java
+++ StudentsTest/src/ExpressionEvaluation.java
@@ -1,6 +1,45 @@
+// Evaluate an expression based on the parameters passed to it
+/**
+ * 
+ * @author Ravi_Kalla
+ *
+ */
 public class ExpressionEvaluation {
 	public static void main(String[] args) {
-		
+		String[] arrData = {"1", "2", "3"};
+		String strExpression = "{1}+{2}={3}";
+		boolean blnExpressionResult = ExpressionEvaluation.evalueateExpression(arrData, strExpression);
+		System.out.println("Result of evaluated expression is = " + blnExpressionResult);
 	}
-	
+
+	public static boolean evalueateExpression(String[] arrData, String strExpression) {
+		// TODO : Write logic to evaluate expression based on the data passed to it
+		return false;
+	}
 }
+
+/*
+Eg1:
+	I/P:
+		arrData = {"10", "2", "12"};
+		strExpression = "{1}+{2}={3}";
+
+	O/P :
+		Result of evaluated expression is = true
+
+Eg2:
+	I/P:
+		arrData = {"10", "2", "11"};
+		strExpression = "{1}+{2}={3}";
+
+	O/P :
+		Result of evaluated expression is = false
+
+Eg3:
+	I/P:
+		arrData = {"10", "2", "11"};
+		strExpression = "({1}+{2})/6=(({3}-1)/({1}/{2}))";
+
+	O/P :
+		Result of evaluated expression is = true
+*/
